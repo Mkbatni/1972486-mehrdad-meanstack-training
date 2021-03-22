da01bf59330f9eb6fe4a2177d3057d31597a1e37
Mike Batani <mkbatani@gmail.com>
2021-03-21 21:13:27 -0700

    the second part is completly done

---
+++ ShopingCart/cart.js
@@ -0,0 +1,33 @@
+function display() {
+    var total = 0;
+    var body = document.getElementsByTagName("tbody")[0];
+    for (var index = 0; index < localStorage.length; index++) {
+        var dt = localStorage.getItem(localStorage.key(index));
+        var st = JSON.parse(dt);
+        console.log(st);
+        console.log(" THIS IS  " + st['itemName'] + st['itemPrice'] + st['quanitity']);
+        var newRow = body.insertRow(0);
+        var cell1 = newRow.insertCell(0);
+        cell1.innerHTML = st['quanitity'];
+        var cell2 = newRow.insertCell(1);
+        cell2.innerHTML = st['itemName'];
+        var cell3 = newRow.insertCell(2);
+        cell3.innerHTML = "$" + st['itemPrice'];
+        var itemP = +st['itemPrice'];
+        var itemQ = +st['quanitity'];
+        total += (itemP * itemQ);
+        console.log(itemQ + " <--> " + itemP);
+        var cell4 = newRow.insertCell(3);
+        cell4.innerHTML = "<a href='#' onclick='deleteRec(this)'>X</a>";
+    }
+    console.log("Total: " + total);
+    var tmp = total.toString();
+    document.getElementById("tt").innerHTML = "<h4>$" + tmp + "</h4>";
+}
+function clearOut() {
+    localStorage.clear();
+    alert("Your cart is clear now, please refresh the page!");
+}
+function pay() {
+    alert("Thank you for shoping! come back soon.");
+}
---
+++ ShopingCart/cart.ts
@@ -0,0 +1,65 @@
+function display()
+{
+
+   var total:number = 0;
+  
+   var body = document.getElementsByTagName("tbody")[0];
+
+
+
+   for (var index = 0; index < localStorage.length; index++) {
+    var dt = localStorage.getItem(localStorage.key(index));
+    var st = JSON.parse(dt);
+    console.log(st);
+    console.log(" THIS IS  " + st['itemName'] + st['itemPrice'] + st['quanitity']);
+  
+   
+    
+        
+
+        var newRow = body.insertRow(0);
+    
+    var cell1 = newRow.insertCell(0);
+    cell1.innerHTML = st['quanitity'] ;
+
+    var cell2 = newRow.insertCell(1); 
+    cell2.innerHTML= st['itemName']  ;
+
+    var cell3 = newRow.insertCell(2);
+    cell3.innerHTML= "$" + st['itemPrice'] ;
+    
+    
+    const itemP:number = +st['itemPrice'];
+    const itemQ:number = +st['quanitity'];
+    total += (itemP * itemQ);
+
+
+    console.log(itemQ + " <--> " + itemP);  
+
+
+    var cell4 = newRow.insertCell(3);
+    cell4.innerHTML="<a href='#' onclick='deleteRec(this)'>X</a>";
+
+}
+
+console.log("Total: " +  total);  
+
+
+const tmp:string = total.toString();
+
+
+
+document.getElementById("tt").innerHTML = "<h4>$" + tmp + "</h4>";
+
+
+
+}
+
+function clearOut()
+{
+    localStorage.clear();
+}
+function pay()
+{
+    alert("Thank you for shoping! come back soon.")
+}
