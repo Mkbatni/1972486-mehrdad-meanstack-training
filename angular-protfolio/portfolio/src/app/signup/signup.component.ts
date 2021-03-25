dd7c379bbb8dcb4cef89ade8589a70a7c929f2b9
Mike Batani <mkbatani@gmail.com>
2021-03-24 21:04:22 -0700

    logic is done

--- angular-protfolio/portfolio/src/app/signup/signup.component.ts
+++ angular-protfolio/portfolio/src/app/signup/signup.component.ts
@@ -6,11 +6,16 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
+msg:string = "";
+
 
   constructor() { }
 
   loginRef = new FormGroup({
+    firstName: new FormControl(),
+    lastName : new FormControl(),
     user: new FormControl(),
     pass: new FormControl()
 
@@ -20,6 +25,49 @@ export class SignupComponent implements OnInit {
   }
   checkUser()
   {
-   console.log(this.loginRef.value) 
+
+
+
+     // obj.pass : this.loginRef.get('pass')
+
+var obj = {
+  firstName: this.loginRef.get('firstName')?.value,
+  lastName : this.loginRef.get('lastName')?.value,
+  user : this.loginRef.get('user')?.value,
+  pass : this.loginRef.get('pass')?.value
+  
+}
+
+
+    
+   
+    let tmpUser = this.loginRef.get('user')?.value;
+    let tmpPass = this.loginRef.get('pass')?.value;
+
+    
+    if(tmpUser != null && tmpPass != null)
+    {
+      const  key:string = this.loginRef.get('user')?.value
+     
+      //console.log(this.loginRef.get("user")?.value || '{}') 
+      localStorage.setItem(key,JSON.stringify(obj));
+    }
+    else{
+      this.msg = "Please type in your user name and password!";
+    }
+    
+  
+    this.loginRef.reset();
   }
+
+
+  DisplayInPrivate()
+  {
+    for (let index = 0; index < localStorage.length; index++) {
+      const element = localStorage.getItem(localStorage.key(index) || '{}' );
+     const  data = JSON.parse(element || '{}');
+      console.log(data.user," " ,data.pass);
+    }
+  }
+
 }
